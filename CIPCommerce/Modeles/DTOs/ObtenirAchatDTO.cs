581589282d45377d5482c2dad230b67caa4bbc5e
RemiThorez <remi.thorez@gmail.com>
2025-03-23 11:23:46 -0400

    DTO Achat fait !

--- CIPCommerce/Modeles/DTOs/ObtenirAchatDTO.cs
+++ CIPCommerce/Modeles/DTOs/ObtenirAchatDTO.cs
@@ -4,8 +4,27 @@
     {
         public ObtenirAchatDTO() { }
 
+        public ObtenirAchatDTO(List<Facture> facturesUtilisateurs) 
+        {
+            LesProduits.Clear();
+            MontantTotal = 0;
+
+            BdContexteCommerce _bd = new BdContexteCommerce();
+            foreach (Facture f in facturesUtilisateurs)
+            {
+                List<FactureProduits> factureProduits = _bd.TableFactureProduits.Where(fp => fp.IdFacture == f.Id).ToList();
+
+                foreach (FactureProduits fp in factureProduits)
+                {
+                    Produit produit = _bd.TableProduit.Find(fp.IdProduit);
+                    LesProduits.Add(new ProduitAchatDTO(produit, fp.Qte, f.DateAchat));
+                    MontantTotal += produit.Prix * fp.Qte;
+                }
+            }
+        }
+
         public double MontantTotal { get; set; }
 
-        public List<Produit> LesProduits { get; set; }
+        public List<ProduitAchatDTO> LesProduits { get; set; }
     }
 }
--- CIPCommerce/Modeles/DTOs/ProduitAchatDTO.cs
+++ CIPCommerce/Modeles/DTOs/ProduitAchatDTO.cs
@@ -4,13 +4,30 @@
     {
         public ProduitAchatDTO() { }
 
+        public ProduitAchatDTO(Produit produit, int qte, DateTime dateAchat) 
+        {
+            Id = produit.Id;
+            Titre = produit.Titre;
+            Prix = produit.Prix;
+            DateAchat = dateAchat;
+            Qte = qte;
+            NomVendeur = produit.Vendeur.Nom;
+            IdVendeur = produit.IdVendeur;
+
+        }
+
         public int Id { get; set; }
 
         public string Titre { get; set; }
 
         public double Prix { get; set; }
 
-        public string Categorie { get; set; }
+        public DateTime DateAchat { get; set; }
+        public int Qte { get; set; }
+
+        public double PrixUnitaire { get; set; }
+
+        public string NomVendeur { get; set; }
 
         public int IdVendeur { get; set; }
     }
