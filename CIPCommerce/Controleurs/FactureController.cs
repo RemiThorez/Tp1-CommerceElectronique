edeab641bf21a04ed8f257bc40a20240b4261c31
RemiThorez <remi.thorez@gmail.com>
2025-03-23 14:18:04 -0400

    Ajout des méthodes dans le controleur

--- CIPCommerce/Controleurs/FactureController.cs
+++ CIPCommerce/Controleurs/FactureController.cs
@@ -1,10 +1,13 @@
 ﻿using CIPCommerce.Modeles;
+using CIPCommerce.Modeles.DTOs;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Net;
 
 namespace CIPCommerce.Controleurs
 {
     [Produces("application/json")]
-    [Route("api/Facture")]
+    [Route("api/facture")]
     [ApiController]
     public class FactureController : ControllerBase
     {
@@ -13,6 +16,52 @@ namespace CIPCommerce.Controleurs
         public FactureController()
         {
             _bd = new BdContexteCommerce();
+        }
+
+        [HttpGet("factures")]
+        [ProducesResponseType(StatusCodes.Status400BadRequest, Type = typeof(BadRequestResult))]
+        [ProducesResponseType(StatusCodes.Status200OK, Type =typeof(ObtenirFactureDTO))]
+        public IActionResult ObtenirFactures()
+        {
+            Utilisateur utilisateurAuth = ControllerContext.HttpContext.Items["Utilisateur"] as Utilisateur;
+
+            if (utilisateurAuth == null)
+            {
+                return BadRequest();
+            }
+            return Ok(new ObtenirVenteDTO(utilisateurAuth.Id));
+        }
+
+        [HttpGet("achats")]
+        [ProducesResponseType(StatusCodes.Status400BadRequest, Type = typeof(BadRequestResult))]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized, Type = typeof(UnauthorizedResult))]
+        [ProducesResponseType(StatusCodes.Status404NotFound, Type = typeof(NotFoundResult))]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(ObtenirAchatDTO))]
+        public IActionResult ObtenirAchats()
+        {
+            Utilisateur utilisateurAuth = ControllerContext.HttpContext.Items["Utilisateur"] as Utilisateur;
+
+            if (utilisateurAuth == null)
+            {
+                return BadRequest();
+            }
+            return Ok(new ObtenirAchatDTO(_bd.TableFacture.Where(f => f.IdAcheteur == utilisateurAuth.Id).ToList()));
+        }
+
+        [HttpGet("ventes")]
+        [ProducesResponseType(StatusCodes.Status400BadRequest, Type =typeof(BadRequestResult))]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized, Type =typeof(UnauthorizedResult))]
+        [ProducesResponseType(StatusCodes.Status404NotFound, Type =typeof(NotFoundResult))]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(ObtenirVenteDTO))]
+        public IActionResult ObtenirVentes()
+        {
+            Utilisateur utilisateurAuth = ControllerContext.HttpContext.Items["Utilisateur"] as Utilisateur;
+
+            if (utilisateurAuth == null)
+            {
+                return BadRequest();
+            }
+            return Ok(new ObtenirVenteDTO(utilisateurAuth.Id));
         }
     }
 }
