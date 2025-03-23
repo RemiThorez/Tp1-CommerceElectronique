20eb83f0414c046946b7791256a96e77e5552d77
RemiThorez <remi.thorez@gmail.com>
2025-03-23 11:05:15 -0400

    Début création DTo achat

--- CIPCommerce/Modeles/DTOs/ObtenirAchatDTO.cs
+++ CIPCommerce/Modeles/DTOs/ObtenirAchatDTO.cs
@@ -2,5 +2,10 @@
 {
     public class ObtenirAchatDTO
     {
+        public ObtenirAchatDTO() { }
+
+        public double MontantTotal { get; set; }
+
+        public List<Produit> LesProduits { get; set; }
     }
 }
---
+++ CIPCommerce/Modeles/DTOs/ProduitAchatDTO.cs
@@ -0,0 +1,17 @@
+﻿namespace CIPCommerce.Modeles.DTOs
+{
+    public class ProduitAchatDTO
+    {
+        public ProduitAchatDTO() { }
+
+        public int Id { get; set; }
+
+        public string Titre { get; set; }
+
+        public double Prix { get; set; }
+
+        public string Categorie { get; set; }
+
+        public int IdVendeur { get; set; }
+    }
+}
