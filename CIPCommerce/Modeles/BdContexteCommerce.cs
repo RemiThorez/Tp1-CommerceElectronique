16def31a98f98b72c507c8cb2898868ed2ef525b
RemiThorez <remi.thorez@gmail.com>
2025-03-23 13:46:41 -0400

    Ajout de BdContexte + correction d'erreur mineures

---
+++ CIPCommerce/Modeles/BdContexteCommerce.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CIPCommerce.Modeles
+{
+    public partial class BdContexteCommerce : DbContext
+    {
+        private string _stringConnexion = "";
+
+        public BdContexteCommerce() { }
+
+        public BdContexteCommerce(string stringConnexion)
+        {
+            _stringConnexion = stringConnexion;
+        }
+
+        public BdContexteCommerce(DbContextOptions<BdContexteCommerce> options) : base(options) {  }
+
+        public virtual DbSet<Utilisateur> TableUtilisateur {  get; set; }
+        public virtual DbSet<Facture> TableFacture { get; set; }
+        public virtual DbSet<FactureProduits> TableFactureProduits { get; set; }
+        public virtual DbSet<Panier> TablePanier { get; set; }
+        public virtual DbSet<Produit> TableProduit { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) => optionsBuilder.UseSqlServer(_stringConnexion);
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
--- CIPCommerce/Modeles/DTOs/ObtenirVenteDTO.cs
+++ CIPCommerce/Modeles/DTOs/ObtenirVenteDTO.cs
@@ -11,12 +11,12 @@
 
             BdContexteCommerce _bd = new BdContexteCommerce();
 
-            List<FactureProduits> factureProduits = _bd.FactureProduits.Where(fp => _bd.Produit.Where(p => p.IdVendeur == idVendeur).Select(p => p.Id).Contains(fp.IdProduit)).ToList();
+            List<FactureProduits> factureProduits = _bd.TableFactureProduits.Where(fp => _bd.TableProduit.Where(p => p.IdVendeur == idVendeur).Select(p => p.Id).Contains(fp.IdProduit)).ToList();
 
             foreach (FactureProduits fp in factureProduits)
             {
                 Produit produit = _bd.TableProduit.Find(fp.IdProduit);
-                LesProduits.Add(new ProduitVenteDTO(produit, fp.Qte, fp.LaFacture.DateAchat, fp.LaFacture.IdAcheteur, _bd.TableUtilisateur.Find(fp.LaFacture.IdAcheteur).Select(u => u.Nom));
+                LesProduits.Add(new ProduitVenteDTO(produit, fp.Qte, fp.LaFacture.DateAchat, fp.LaFacture.IdAcheteur, _bd.TableUtilisateur.Find(fp.LaFacture.IdAcheteur).Nom));
                 MontantTotal += produit.Prix * fp.Qte;
             }
         }
@@ -26,4 +26,4 @@
         public List<ProduitVenteDTO> LesProduits { get; set; }
     }
 }
-}
+
