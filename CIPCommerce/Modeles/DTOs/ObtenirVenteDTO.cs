fd3135e2c074a66c7d6c8aa51026e444d6f89ef1
RemiThorez <remi.thorez@gmail.com>
2025-03-23 13:33:50 -0400

    DTO de vente fait

--- CIPCommerce/Modeles/DTOs/ObtenirVenteDTO.cs
+++ CIPCommerce/Modeles/DTOs/ObtenirVenteDTO.cs
@@ -2,5 +2,28 @@
 {
     public class ObtenirVenteDTO
     {
+        public ObtenirVenteDTO() { }
+
+        public ObtenirVenteDTO(int idVendeur)
+        {
+            LesProduits.Clear();
+            MontantTotal = 0;
+
+            BdContexteCommerce _bd = new BdContexteCommerce();
+
+            List<FactureProduits> factureProduits = _bd.FactureProduits.Where(fp => _bd.Produit.Where(p => p.IdVendeur == idVendeur).Select(p => p.Id).Contains(fp.IdProduit)).ToList();
+
+            foreach (FactureProduits fp in factureProduits)
+            {
+                Produit produit = _bd.TableProduit.Find(fp.IdProduit);
+                LesProduits.Add(new ProduitVenteDTO(produit, fp.Qte, fp.LaFacture.DateAchat, fp.LaFacture.IdAcheteur, _bd.TableUtilisateur.Find(fp.LaFacture.IdAcheteur).Select(u => u.Nom));
+                MontantTotal += produit.Prix * fp.Qte;
+            }
+        }
+
+        public double MontantTotal { get; set; }
+
+        public List<ProduitVenteDTO> LesProduits { get; set; }
     }
+}
 }
---
+++ CIPCommerce/Modeles/DTOs/ProduitVenteDTO.cs
@@ -0,0 +1,33 @@
+﻿namespace CIPCommerce.Modeles.DTOs
+{
+    public class ProduitVenteDTO
+    {
+        public ProduitVenteDTO() { }
+
+        public ProduitVenteDTO(Produit produit, int qte, DateTime dateAchat, int idAcheteur, string nomAcheteur)
+        {
+            Id = produit.Id;
+            Titre = produit.Titre;
+            Prix = produit.Prix;
+            DateAchat = dateAchat;
+            Qte = qte;
+            NomAcheteur = nomAcheteur;
+            IdAcheteur = idAcheteur;
+        }
+
+        public int Id { get; set; }
+
+        public string Titre { get; set; }
+
+        public double Prix { get; set; }
+
+        public DateTime DateAchat { get; set; }
+        public int Qte { get; set; }
+
+        public double PrixUnitaire { get; set; }
+
+        public string NomAcheteur { get; set; }
+
+        public int IdAcheteur { get; set; }
+    }
+}
