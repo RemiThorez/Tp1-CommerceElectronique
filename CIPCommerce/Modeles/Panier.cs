a011cadecb11952a7b49b03d0128e66105ae82f5
RemiThorez <remi.thorez@gmail.com>
2025-03-23 09:34:15 -0400

    Classe représentant la table Panier est fait

--- CIPCommerce/Modeles/Panier.cs
+++ CIPCommerce/Modeles/Panier.cs
@@ -1,6 +1,21 @@
-﻿namespace CIPCommerce.Modeles
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CIPCommerce.Modeles
 {
-    public class Panier
+    public partial class Panier
     {
+        public int Id { get; set; }
+
+        public int IdAcheteur {  get; set; }
+
+        public int IdProduit { get; set; }
+
+        public int Qte {  get; set; }
+
+        [ForeignKey("IdAcheteur")]
+        public Utilisateur Acheteur { get; set; }
+
+        [ForeignKey("IdProduit")]
+        public Produit LeProduit { get; set; }
     }
 }
