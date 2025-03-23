7a95d6e8b379e028540913ccfb099049b97f0a55
RemiThorez <remi.thorez@gmail.com>
2025-03-23 09:30:53 -0400

    Classe représentant la table produit est fait

--- CIPCommerce/Modeles/Produit.cs
+++ CIPCommerce/Modeles/Produit.cs
@@ -1,6 +1,22 @@
-﻿namespace CIPCommerce.Modeles
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CIPCommerce.Modeles
 {
-    public class Produit
+    public partial class Produit
     {
+        public int Id { get; set; }
+
+        public string Description { get; set; }
+
+        public double Prix { get; set; }
+
+        public string Categorie {  get; set; }
+
+        public string Image {  get; set; }
+        
+        public int IdVendeur {  get; set; }
+
+        [ForeignKey("IdVendeur")]
+        public Utilisateur Vendeur { get; set; }
     }
 }
