f29369cd0e798a18f87b0deaf6e77dc77f750ae9
RemiThorez <remi.thorez@gmail.com>
2025-03-23 10:21:38 -0400

    CreationUsagerDTO fait

--- CIPCommerce/Modeles/DTOs/CreationUsagerDTO.cs
+++ CIPCommerce/Modeles/DTOs/CreationUsagerDTO.cs
@@ -2,5 +2,25 @@
 {
     public class CreationUsagerDTO
     {
+        public CreationUsagerDTO() { }
+
+        public Utilisateur RetoutrnerUtilisateur()
+        {
+            Utilisateur utilisateur = new Utilisateur();
+
+            utilisateur.Nom = Nom;
+            utilisateur.Identifiant = Identifiant;
+            utilisateur.Mdp = Mdp;
+            utilisateur.Role = Role;
+
+            return utilisateur;
+        }
+        public string Nom { get; set; }
+
+        public string Identifiant { get; set; }
+
+        public string Mdp { get; set; }
+
+        public bool Role { get; set; }
     }
 }
