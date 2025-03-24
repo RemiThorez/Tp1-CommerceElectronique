22175655ae7220fca5a35768ac38933c8c6c5904
RemiThorez <remi.thorez@gmail.com>
2025-03-23 22:36:24 -0400

    Ajout d'une migration

--- CIPCommerce/Migrations/20250324023605_test3.Designer.cs
+++ CIPCommerce/Migrations/20250324023605_test3.Designer.cs
@@ -4,6 +4,7 @@ using CIPCommerce.Modeles;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CIPCommerce.Migrations
 {
     [DbContext(typeof(BdContexteCommerce))]
-    partial class BdContexteCommerceModelSnapshot : ModelSnapshot
+    [Migration("20250324023605_test3")]
+    partial class test3
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ CIPCommerce/Migrations/20250324023605_test3.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CIPCommerce.Migrations
+{
+    /// <inheritdoc />
+    public partial class test3 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "Actif",
+                table: "TableUtilisateur",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "EnVente",
+                table: "TableProduit",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Actif",
+                table: "TableUtilisateur");
+
+            migrationBuilder.DropColumn(
+                name: "EnVente",
+                table: "TableProduit");
+        }
+    }
+}
